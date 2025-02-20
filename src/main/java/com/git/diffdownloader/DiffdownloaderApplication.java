03e32c6469cb3a388bfdb725fa8ebfea9a5d962f
hansol.kim <hansol.kim@bankwareglobal.com>
2025-02-20 23:24:15 +0900

    최신 태그 가져오는 기능 추가

--- src/main/java/com/git/diffdownloader/DiffdownloaderApplication.java
+++ src/main/java/com/git/diffdownloader/DiffdownloaderApplication.java
@@ -1,23 +1,28 @@
 package com.git.diffdownloader;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
-public class DiffdownloaderApplication implements ApplicationRunner{
+public class DiffdownloaderApplication implements ApplicationRunner {
 
 	private final String defaultPath = "file:./application.properties";
 
 	private final ConfigurableEnvironment environment;
+	
+	private final ApplicationContext context;
 
-    public DiffdownloaderApplication(ConfigurableEnvironment environment) {
-        this.environment = environment;
-    }
 
+	public DiffdownloaderApplication(ConfigurableEnvironment environment, ApplicationContext context) {
+		this.environment = environment;
+		this.context = context;
+	}
 
 	public static void main(String[] args) {
 		// args 체크하기
@@ -25,9 +30,9 @@ public class DiffdownloaderApplication implements ApplicationRunner{
 		SpringApplication.run(DiffdownloaderApplication.class, args);
 	}
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
 		String mode = "";
 		// e 로 main 인지 release 인지 확인
 		// main 인지 release 인지에 따라 다른 프로퍼티 읽음
@@ -39,21 +44,15 @@ public class DiffdownloaderApplication implements ApplicationRunner{
 			throw new Exception("No env argument");
 		}
 
-		String configPath = args.getOptionValues("p") != null && !args.getOptionValues("p").isEmpty()
-                ? "file:" + args.getOptionValues("p").get(0)
-                : defaultPath;
+//		String configPath = args.getOptionValues("p") != null && !args.getOptionValues("p").isEmpty()
+//				? "file:" + args.getOptionValues("p").get(0)
+//				: defaultPath;
+//
+//		System.setProperty("spring.config.location", configPath);
+//		((StandardEnvironment) environment).getSystemProperties().put("spring.config.location", configPath);
 
-		System.setProperty("spring.config.location",  configPath);
-		((StandardEnvironment) environment).getSystemProperties()
-		.put("spring.config.location", configPath);
-
-		
-		// 최신 태그 가져오기
-
-
-		// 
-    }
-
-	
+		DeliveryService deliveryService = (DeliveryService) context.getBean("mainDeliveryService");
+		deliveryService.makeZip();
+	}
 
 }
--- src/main/java/com/git/diffdownloader/MainDeliveryServiceImpl.java
+++ src/main/java/com/git/diffdownloader/MainDeliveryServiceImpl.java
@@ -5,18 +5,77 @@
 
 package com.git.diffdownloader;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
  * @author codespace
  */
-@Service
+@Service(value = "mainDeliveryService")
 public class MainDeliveryServiceImpl implements DeliveryService {
 
-    @Override
-    public void makeZip() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+	private final RestTemplate restTemplate;
 
+	@Value("${gitlab.project.id}")
+	private String[] projectIds;
+
+	@Value("${gitlab.api.url}")
+	String gitlabApiUrl;
+
+	@Value("${gitlab.token}")
+	String gitlabToken;
+
+	public MainDeliveryServiceImpl(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@Override
+	public void makeZip() {
+
+		List<String> latestTags = new ArrayList<String>();
+		// get current tag
+		for (String projectId : projectIds) {
+			String lastestTag = getLatestTags(projectId);
+			if (lastestTag != null) {
+				latestTags.add(getLatestTags(projectId));
+			}
+		}
+
+		System.out.println(Arrays.toString(latestTags.toArray()));
+
+		// compare
+
+		// download
+	}
+
+	private String getLatestTags(String projectId) {
+
+		String url = String.format("%s/projects/%s/repository/tags", gitlabApiUrl, projectId);
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("PRIVATE-TOKEN", gitlabToken);
+		
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+		ResponseEntity<Map[]> response = restTemplate.exchange(
+	            url, 
+	            HttpMethod.GET, 
+	            entity, 
+	            Map[].class, 
+	            projectId
+	        );
+		
+		List<Map> tags = Arrays.asList(response.getBody());
+		return tags.isEmpty() ? "No tags found" : (String) tags.get(0).get("name");
+
+	}
 }
